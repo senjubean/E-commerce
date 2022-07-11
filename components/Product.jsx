6a692c65143fd9e23f48673a084ac3f7b118ebb6
Dinesh Ban <eternalsharinganeye@gmail.com>
2022-07-11 21:09:27 +0545

    add cart functionality

--- components/Product.jsx
+++ components/Product.jsx
@@ -6,7 +6,7 @@ const Product = ({product:{image, name, slug, price}}) => {
   return (
     <div>
         <Link href={`/product/${slug.current}`}>
-            <div className="product-cart">
+            <div className="product-card">
                 <img src={urlFor(image && image[0])} alt="" width={250} height={250} className="product-image" />
                 <p className="product-name">{name}</p>
                 <p className="product$-price">${price}</p>
